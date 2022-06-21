510d6405cc810d630dc04210df41b202a4bac62a
Venkatesh Sai <aeronut1994@gmail.com>
2022-06-21 07:07:50 +0530

    Updated Max Angle to rescale control
    Changed / increased max angle

--- ZuppaNavgatiZAIPSDK/ZuppaNavgatiZAIPSDK/MainModule.py
+++ ZuppaNavgatiZAIPSDK/ZuppaNavgatiZAIPSDK/MainModule.py
@@ -4,7 +4,7 @@ import ZuppaNavgatiSDK as sdk
 
 #print(int((Constants.WL_CALIBRATION.NO_CALIBRATION)),type(Constants.WL_CALIBRATION.NO_CALIBRATION))
 
-#AP(SECONDARY SLAVE) <-> COMPANION COMPUTER(SLAVE) < - > GCS(MaSTER) 
+#AP(SECONDARY SLAVE) <-> COMPANION COMPUTER(SLAVE) < - > GCS(MaSTER)
 #port=sdk.PrimaryInterface("COM24","COM25") # <Autopilot com port> , <Telemetry module com Port>
 
 #AP(SLAVE) <-> COMPANION COMPUTER(MASTER)
@@ -12,29 +12,29 @@ port=sdk.PrimaryInterface("COM8",None) # <Autopilot com port> , <Telemetry modul
 
 #-------------------APPLICATION CODE-----------------
 def pwmToDeg(inputPwm):
-    rel=((inputPwm-1500)/500)*70;
+    rel=((inputPwm-1500)/500)*75;
     return rel
 tog=0
 tg=1;
 cntr=0;
 mainCntr=0;
 while(True):
-    
+
     #print("Check: ",port.forwardingTimeout);
-    #if(port.forwardingTimeout==True): # CONVERTS Schema from 
+    #if(port.forwardingTimeout==True): # CONVERTS Schema from
     #    port.usingForwarding=False;   # AP(SECONDARY SLAVE) <-> COMPANION COMPUTER(SLAVE) <-> GCS(MaSTER)  TO   AP(SLAVE) <-> COMPANION COMPUTER(MASTER)
-    
+
     #port.fwdConn.write("DaTA FROM VISION SYSTEM");
 
     # Common Access port.wlInterface.vehicle
-    # Roll - 15 , pitch - 10 , yaw - 20 dps 
-   
+    # Roll - 15 , pitch - 10 , yaw - 20 dps
+
     #FOR DIRECT ROLL < PITCH YAW
    # port.wlInterface.vehicle.setFlightMode(Constants.FLIGHT_MODES.EZ_FLY);                 # FIXED WING -- heading hold
   #  port.wlInterface.vehicle.vehicleControl.assignControl(pwmToDeg(1700),pwmToDeg(1700),20,1500); # FIXED WING  #20HZ can be given
-    
-    
-    
+
+
+
     # FOR TARGET POINT METHOD
    # port.wlInterface.vehicle.setFlightMode(Constants.FLIGHT_MODES.NAVIGATION);    # FIXED WING
    # port.wlInterface.vehicle.setFlightMode(Constants.FLIGHT_MODES.POSITION_HOLD); # MULTI COPTER
@@ -62,7 +62,7 @@ while(True):
             if(cntr<-45):
                 tg=0
     else:
- 
+
         port.wlInterface.vehicle.vehicleControl.withDrawControl();
         port.wlInterface.vehicle.setFlightMode(Constants.FLIGHT_MODES.MANUAL);
         #
